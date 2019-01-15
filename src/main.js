658d298bf5e3bb69b3051f901965294f3c0cbc29
Uk <ukchukundah@gmail.com>
2019-01-15 15:56:22 +0100

    Install axios

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import iView from 'iview';
+import axios from 'axios';
 import 'iview/dist/styles/iview.css';
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
 Vue.use(iView);
+window.axios = axios;
+axios.defaults.baseURL = 'http://127.0.0.1:3333';
 
 /* eslint-disable no-new */
 new Vue({
