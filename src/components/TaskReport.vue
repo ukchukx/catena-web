8f70f494b4ac0c1b7ddc07fea0b156b9ad7bc202
Uk <ukchukundah@gmail.com>
2019-01-22 22:55:35 +0100

    Show completed, missed and upcoming tasks in calendar

--- src/components/TaskReport.vue
+++ src/components/TaskReport.vue
@@ -4,19 +4,14 @@
     <ProfileLink/>
     <div class="row mt-4">
       <div class="col-sm-12">
-        <h4>{{ task.name }}</h4>
+        <h3>{{ task.name }}</h3>
       </div>
       <div class="col-sm-12 mt-3">
         <h6>Current streak: 0</h6>
         <h6>Longest streak: 0</h6>
       </div>
-      <div class="col-sm-12">
-        <Vuecal
-          class="vuecal--rounded-theme"
-          hide-view-selector
-          :time="false"
-          default-view="month"
-        />
+      <div class="col-sm-12 col-md-6 mx-auto mt-4">
+        <v-calendar :is-expanded="true" :attributes="events"/>
       </div>
     </div>
   </div>
@@ -24,14 +19,11 @@
 
 <script>
 import { mapGetters } from 'vuex';
-import Vuecal from 'vue-cal';
-import 'vue-cal/dist/vuecal.css';
 import ProfileLink from '@/components/ProfileLink';
 
 export default {
   name: 'TaskReport',
   components: {
-    Vuecal,
     ProfileLink
   },
   computed: {
@@ -39,7 +31,47 @@ export default {
     task() {
       const { params: { id } } = this.$route;
       return this.tasks.find(t => t.id === new Number(id).valueOf());
+    },
+    events() {
+      return this.task.schedules
+        .map((schedule) => {
+          const today = new Date();
+          today.setUTCHours(0, 0, 0, 0);
+
+          const date = new Date(schedule.due_date);
+          date.setUTCHours(0, 0, 0, 0);
+
+          let highlight;
+          if (schedule.done) { // done
+            highlight = {
+              backgroundColor: '#66b3cc',
+              borderColor: '#53a9c6',
+              borderWidth: '2px'
+            };
+          } else if (today <= date) { // upcoming
+            highlight = {
+              backgroundColor: '#8B8C8B',
+              borderColor: '#333333',
+              borderWidth: '2px'
+            };
+          } else { // missed
+            highlight = {
+              backgroundColor: '#ff8080',
+              borderColor: '#ff6666',
+              borderWidth: '2px'
+            };
+          }
+
+          return {
+            contentStyle: { color: 'white' },
+            highlight,
+            dates: date,
+            key: schedule.id,
+            customData: schedule
+          };
+        });
     }
   }
 };
 </script>
+
--- src/main.js
+++ src/main.js
@@ -4,16 +4,22 @@ import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
 import VueFlashMessage from 'vue-flash-message';
 import axios from 'axios';
+import { setupCalendar, Calendar } from 'v-calendar';
 import 'bootstrap/dist/css/bootstrap.css'; // eslint-disable-line import/no-extraneous-dependencies
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import 'vue-flash-message/dist/vue-flash-message.min.css';
+import 'v-calendar/lib/v-calendar.min.css';
 import App from './App';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+
 Vue.use(BootstrapVue);
+setupCalendar({ locale: 'en' });
+Vue.component('v-calendar', Calendar);
 Vue.use(VueFlashMessage, { createShortcuts: false });
+
 window.axios = axios;
 axios.defaults.baseURL = 'http://127.0.0.1:3333/api/v1';
 axios.interceptors.request.use((config) => {
