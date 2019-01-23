b9f2d77360ff4bea77664a02b007ba7edc8ce33c
Uk <ukchukundah@gmail.com>
2019-01-23 10:10:25 +0100

    Show current and longest streak

--- src/components/TaskReport.vue
+++ src/components/TaskReport.vue
@@ -3,14 +3,18 @@
   <div>
     <ProfileLink/>
     <div class="row mt-4">
-      <div class="col-sm-12">
-        <h3>{{ task.name }}</h3>
+      <div class="col-sm-12 col-md-4 mb-3">
+        <h1 class="task-name">{{ task.name }}</h1>
+        <p class="sub-text text-muted mt-3">
+          <span class="h2">Current streak</span>
+          <span class="streak font-weight-bold">{{ currentStreak }}</span>
+        </p>
+        <p class="sub-text text-muted">
+          <span class="h2">Longest streak</span>
+          <span class="streak font-weight-bold">{{ longestStreak }}</span>
+        </p>
       </div>
-      <!-- <div class="col-sm-12 mt-3">
-        <h6>Current streak: 0</h6>
-        <h6>Longest streak: 0</h6>
-      </div>-->
-      <div class="col-sm-12 col-md-6 mx-auto mt-4">
+      <div class="col-sm-12 col-md-8">
         <v-calendar :is-expanded="true" :attributes="events"/>
       </div>
     </div>
@@ -32,23 +36,72 @@ export default {
       const { params: { id } } = this.$route;
       return this.tasks.find(t => t.id === new Number(id).valueOf());
     },
+    today() {
+      const today = new Date();
+      today.setUTCHours(0, 0, 0, 0);
+      return today;
+    },
+    currentStreak() {
+      const result = [];
+      /* eslint-disable no-plusplus */
+      for (let d = this.streakable.length - 1; d >= 0; --d) {
+        if (this.streakable[d].done) {
+          result.push(this.streakable[d]);
+        } else {
+          break;
+        }
+      }
+
+      return result.length;
+    },
+    longestStreak() {
+      const streaks = [];
+      /* eslint-disable no-plusplus */
+      for (let d = 0; d < this.streakable.length; ++d) {
+        const x = [];
+        while (this.streakable[d] && this.streakable[d].done) {
+          x.push(this.streakable[d]);
+          ++d;
+        }
+        streaks.push(x);
+      }
+
+      return Math.max(...streaks.map(streak => streak.length));
+    },
+    streakable() {
+      return this.task.schedules
+        .filter(({ due_date, done }) => {
+          const date = new Date(due_date);
+          date.setUTCHours(0, 0, 0, 0);
+
+          const isToday = +this.today === +date && !done;
+
+          return isToday || this.today >= date;
+        });
+    },
     events() {
       return this.task.schedules
         .map((schedule) => {
-          const today = new Date();
-          today.setUTCHours(0, 0, 0, 0);
-
           const date = new Date(schedule.due_date);
           date.setUTCHours(0, 0, 0, 0);
 
+          const isToday = +this.today === +date && !schedule.done;
+
           let highlight;
+          let dot;
           if (schedule.done) { // done
             highlight = {
               backgroundColor: '#66b3cc',
               borderColor: '#53a9c6',
               borderWidth: '2px'
             };
-          } else if (today <= date) { // upcoming
+          } else if (isToday) { // today
+            dot = {
+              backgroundColor: '#8B8C8B',
+              borderColor: '#333333',
+              borderWidth: '2px'
+            };
+          } else if (this.today <= date) { // upcoming
             highlight = {
               backgroundColor: '#8B8C8B',
               borderColor: '#333333',
@@ -62,16 +115,36 @@ export default {
             };
           }
 
-          return {
+          const obj = {
             contentStyle: { color: 'white' },
             highlight,
             dates: date,
             key: schedule.id,
             customData: schedule
           };
+          const highlightStyle = { highlight, contentStyle: { color: 'white' } };
+          const dotStyle = { dot, contentStyle: { color: 'black' } };
+
+          return !isToday ? Object.assign(obj, highlightStyle) : Object.assign(obj, dotStyle);
         });
     }
   }
 };
 </script>
+<style scoped>
+.task-name {
+  font-size: 3.5rem;
+  font-weight: 500;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+.sub-text {
+  font-size: 2rem !important;
+  font-weight: 400;
+}
+.streak {
+  font-size: 3.5rem;
+}
+</style>
 
