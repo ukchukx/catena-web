d388e554d77c357e5c6cb0a1238a637d925625fd
Uk <ukchukundah@gmail.com>
2020-01-07 10:39:19 +0100

    Fix generator not returning current date (if eligible). The cause was not cloning date at L14 like we did at L19. Closes #15.

--- src/utils/dateGenerator.js
+++ src/utils/dateGenerator.js
@@ -1,21 +1,21 @@
-export default function* dateGenerator({ selectedDays = [], end = null, start = new Date() }) {
-  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+export default function* dateGenerator({ selectedDays = [], end = null, start = new Date() }) {  
   start.setUTCHours(12, 0, 0, 0);
-
   const today = new Date();
-
-  const startIsAfterToday = () => start.getTime() >= today.getTime();
-
+  const nextDate = new Date(start);
   if (!end) end = new Date(start.getFullYear(), 11, 31);
   end.setUTCHours(12, 0, 0, 0);
 
-  if (selectedDays.length && startIsAfterToday()) {
-    if (selectedDays.indexOf(days[start.getDay()]) !== -1) yield start;
-  }
+  const shouldReturn = (date) => {
+    const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    // True if `date` is after `today` and `date`'s day is in `selectedDays`
+    return +date >= +today && selectedDays.indexOf(days[date.getDay()]) !== -1;
+  };
 
-  while (start <= end) {
-    start.setDate(start.getDate() + 1);
-    if (!startIsAfterToday() || (selectedDays.length && selectedDays.indexOf(days[start.getDay()]) === -1)) continue;
-    yield new Date(start);
+  if (shouldReturn(nextDate)) yield new Date(nextDate);
+
+  while (nextDate < end) {
+    nextDate.setTime(nextDate.getTime() + 86400000);
+
+    if (shouldReturn(nextDate)) yield new Date(nextDate);
   }
 }
