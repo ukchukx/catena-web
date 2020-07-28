b659adfa98e58aef61a15b27e5099864f5962ba6
Uk Chukundah <ukchukundah@gmail.com>
2020-07-28 13:37:38 +0100

    Fix today date omission

--- src/utils/dateGenerator.js
+++ src/utils/dateGenerator.js
@@ -1,14 +1,16 @@
-export default function* dateGenerator({ selectedDays = [], end = null, start = new Date() }) {  
-  start.setUTCHours(12, 0, 0, 0);
+export default function* dateGenerator({ selectedDays = [], end = null, start = new Date() }) {
   const today = new Date();
+  today.setUTCHours(0, 0, 0, 0);
+  today.setHours(0);
   const nextDate = new Date(start);
+  nextDate.setUTCHours(12, 0, 0, 0);
   if (!end) end = new Date(start.getFullYear(), 11, 31);
   end.setUTCHours(12, 0, 0, 0);
 
   const shouldReturn = (date) => {
     const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     // True if `date` is after `today` and `date`'s day is in `selectedDays`
-    return +date >= +today && selectedDays.indexOf(days[date.getDay()]) !== -1;
+    return +date >= +today && selectedDays.includes(days[date.getDay()]);
   };
 
   if (shouldReturn(nextDate)) yield new Date(nextDate);
