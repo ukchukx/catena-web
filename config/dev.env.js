fcafd87dc7c2dfcb47a3a64f25e524d5a8c87be2
Uk Chukundah <ukchukundah@gmail.com>
2020-11-16 06:51:43 +0100

    Change dev port

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_HOST: '"http://localhost:4000"',
-  WS_HOST: '"ws://localhost:4000/socket"'
+  API_HOST: '"http://localhost:4001"',
+  WS_HOST: '"ws://localhost:4001/socket"'
 })
--- config/test.env.js
+++ config/test.env.js
@@ -4,6 +4,6 @@ const devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
   NODE_ENV: '"testing"',
-  API_HOST: '"http://localhost:4000"',
-  WS_HOST: '"ws://localhost:4000/socket"'
+  API_HOST: '"http://localhost:4001"',
+  WS_HOST: '"ws://localhost:4001/socket"'
 })
